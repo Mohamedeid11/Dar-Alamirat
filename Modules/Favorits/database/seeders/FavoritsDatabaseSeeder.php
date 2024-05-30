4c609b9a6ecdfe6ca39582acffb4d29dab33d3c6
Mohamed Eid <m.mohamedeid11@gmail.com>
2024-05-30 17:10:48 +0300

    work with favorits

---
+++ Modules/Favorits/database/seeders/FavoritsDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Favorits\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class FavoritsDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/Favorits/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('favorits::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('favorits.name') !!}</p>
+@endsection
