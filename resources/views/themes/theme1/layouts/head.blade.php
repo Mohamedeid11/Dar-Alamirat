cf676085b3ac6453959cdc69fd053da89d406133
Mohamed Eid <m.mohamedeid11@gmail.com>
2024-05-30 17:05:06 +0300

    some edits

--- resources/views/themes/theme1/layouts/head.blade.php
+++ resources/views/themes/theme1/layouts/head.blade.php
@@ -9,12 +9,12 @@
 
     <!-- bootstrap -->
     <link rel="stylesheet" href="{{asset('theme1-assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('theme1-assets/css/bootstrap.rtl.min.css')}}">
     <!-- libs -->
     <link rel="stylesheet" href="{{asset('theme1-assets/css/swiper-bundle.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.8/jquery.jgrowl.min.css" />
+    <link rel="stylesheet" href="{{asset('theme1-assets/css/jquery.jgrowl.min.css')}}">
     <link rel="stylesheet" href="{{asset('theme1-assets/css/sweetalert2.min.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/intl-tel-input@20.2.0/build/css/intlTelInput.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <!-- custom style -->
     <link rel="stylesheet" href="{{asset('theme1-assets/css/style.min.css')}}">
 
