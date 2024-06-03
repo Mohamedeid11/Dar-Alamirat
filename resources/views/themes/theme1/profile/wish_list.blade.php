d99e85743bf735c15c27c7b2a165c4033f9d765f
Mohamed Eid <m.mohamedeid11@gmail.com>
2024-06-03 18:48:41 +0300

    working with favorietes

---
+++ resources/views/themes/theme1/profile/wish_list.blade.php
@@ -0,0 +1,54 @@
+@extends('themes.theme1.layouts.app')
+
+@section('crumbs')
+    <!-- breadcrumbs -->
+    <section class="category-crumb">
+        <!-- container-->
+        <div class="pixel-container">
+            <!-- row -->
+            <div class="wrap">
+                <!-- content -->
+                <ul class="breadcrumbs">
+                    <li>
+                        <a href="{{route('index')}}">
+                            <span>Home</span>
+                        </a>
+                    </li>
+                    <li>
+                        <span> Wish List</span>
+                    </li>
+                </ul>
+                <!-- ./content -->
+            </div>
+            <!-- ./row -->
+        </div>
+        <!-- ./container-->
+    </section>
+    <!-- breadcrumbs -->
+@endsection
+
+@section('content')
+
+    <!-- user-layout -->
+    <section class="user-page-layout">
+        <div class="pixel-container">
+            <div class="wrap">
+                <div class="user-layout">
+                    @include('themes.theme1.profile.profile_aside')
+                    <main>
+                        <h1>Wishlist</h1>
+                        <div class="whishlist_items">
+                            @foreach($favorites as $product)
+                                @include('themes.theme1.partials.item')
+                            @endforeach
+                        </div>
+                    </main>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('scripts')
+
+@endsection
